d75187681d939cb64344263b0b9e3f1062008407
Jaime Berrios <jaime.berrios@grupolorena.local>
2024-04-25 14:22:48 -0600

    Todos los archivos actualizados

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+// import { useCart } from '../hooks/useCart';
+// import { useFilters } from '../hooks/useFilters';
+import './Footer.css'
+
+export function Footer () {
+  // const { filters } = useFilters();
+  // const { cart } = useCart();
+  return (
+    <footer className='footer'>
+      <h4><span>Jaime Berrios</span></h4>
+      <h5>Mi Tienda</h5>
+    </footer>
+  )
+}
